b3b906730a9fce2ac9c8a319754c9fe57610fe47
Mateusz <mkluszczynski07@gmail.com>
2023-11-02 23:13:35 +0100

    Added new log message style

--- src/DBImporter.py
+++ src/DBImporter.py
@@ -1,3 +1,4 @@
+import sys
 from unidecode import unidecode
 from colorama import Fore
 from lib.Repository.DBRepository import DBRepository
@@ -19,6 +20,7 @@ class DBImporter:
         data = csvRepo.find()
         self.__dbRepo.createTable(tableName, [self.__trimColumnName(column) for column in csvRepo.getColumns()])
         print(f"{Fore.CYAN}Inserting {Fore.GREEN}{file_path} {Fore.CYAN}file...")
+        insertedNo = 0
         for row in data:
             rowList = self.__prepareInsertData(row)
             self.__dbRepo.add(
@@ -27,8 +29,16 @@ class DBImporter:
                         "into": tableName
                     }
                 )
+            insertedNo += 1
+            self.__logInsertMessage(str(insertedNo), file_path, tableName)
+            
+        sys.stdout.write("\n")
+            
 
-        print(f"{Fore.CYAN}Inserted data from {Fore.GREEN}{file_path} {Fore.CYAN}to {Fore.GREEN}{tableName} {Fore.CYAN}table.")
+        # print(f"{Fore.CYAN}Inserted data from {Fore.GREEN}{file_path} {Fore.CYAN}to {Fore.GREEN}{tableName} {Fore.CYAN}table.")
+    def __logInsertMessage(self, value: str, file: str, table: str):
+        sys.stdout.write(("\r{0} rows " + Fore.CYAN + "from " + Fore.GREEN + file + Fore.CYAN + " to " + Fore.GREEN + table + Fore.CYAN + " table.").format(f"{Fore.CYAN}Inserted {Fore.MAGENTA}" + value))
+        sys.stdout.flush()
 
     def __prepareInsertData(self, row: dict):
         return [{self.__trimColumnName(column): f"'{self.__replaceSpecialCharacters(value, ' ')}'"} for column, value in row.items() if column != ""]
@@ -37,7 +47,7 @@ class DBImporter:
         return self.__replaceSpecialCharacters(columnName, "S")
 
     def __replaceSpecialCharacters(self, text: str, replace: str):
-        banned = ["'", "%", "!", "?", "%", "@", "$", "^", "&", "-"]
+        banned = ["'", "%", "!", "?", "%", "@", "$", "^", "&", "-", "(", ")"]
         final = unidecode(text).replace(" ", "")
         for ban in banned:
             final = final.replace(ban, replace)
