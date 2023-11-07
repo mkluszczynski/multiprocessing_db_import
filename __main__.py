f6b4ca857916cd0b9bdc36b57a277f4478e8e737
drodu02 <olivier.drozdowski02@gmail.com>
2023-11-07 18:02:51 +0100

    Adding multiprocessing implementation to __main__.py

--- __main__.py
+++ __main__.py
@@ -1,8 +1,9 @@
 from os import listdir
-
+from multiprocessing import Pool
 from lib.Repository.DBRepository import DBRepository
 from utils.Config import Config
 from src.DBImporter import DBImporter
+import time
 
 dbRepo = DBRepository(
     Config.getDbHost(),
@@ -16,7 +17,24 @@ dbImporter = DBImporter(dbRepo)
 
 data_files = listdir("data")
 
-for file in data_files:
-    if file == ".gitkeep": continue
-    print(file)
-    dbImporter.importDataFromCsvFile("data/" + file)
+def importig_data(data_files):
+    for file in data_files:
+        if file == ".gitkeep": continue
+        print(file)
+        dbImporter.importDataFromCsvFile("data/" + file)
+
+
+
+if __name__ == "__main__":
+    start = time.time()
+    importig_data()
+    end = time.time()
+    print(f"Time taken without multiprocessing: {end-start}")
+    
+    time.sleep(1,5)
+
+    start = time.time()
+    pool = Pool()
+    pool.map(importig_data, data_files)
+    end = time.time()
+    print(f"Time taken using multiprocessing: {end-start}")
