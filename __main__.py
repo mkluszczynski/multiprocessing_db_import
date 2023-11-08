8c055c4f17f8b65d775adf97c1153736bc341b4b
Mateusz <mkluszczynski07@gmail.com>
2023-11-08 03:19:52 +0100

    Added multiprocessing

--- __main__.py
+++ __main__.py
@@ -1,22 +1,27 @@
 from os import listdir
+from time import sleep
+from multiprocessing import Pool
 
 from lib.Repository.DBRepository import DBRepository
 from utils.Config import Config
 from src.DBImporter import DBImporter
 
-dbRepo = DBRepository(
-    Config.getDbHost(),
-    Config.getDbPort(),
-    Config.getDbUser(),
-    Config.getDbPassword(),
-    Config.getDbName()
-)
+data_files_names = listdir("data")
+data_files = list(map(lambda file_name: f"data/{file_name}", data_files_names))
 
-dbImporter = DBImporter(dbRepo)
+def import_csv (file_path: str):
+    if file_path == ".gitkeep": return 
 
-data_files = listdir("data")
+    dbRepo = DBRepository(
+        Config.getDbHost(),
+        Config.getDbPort(),
+        Config.getDbUser(),
+        Config.getDbPassword(),
+        Config.getDbName()
+    )
+    dbImporter = DBImporter(dbRepo)
+    dbImporter.importDataFromCsvFile(file_path, False)
 
-for file in data_files:
-    if file == ".gitkeep": continue
-    print(file)
-    dbImporter.importDataFromCsvFile("data/" + file)
+
+pool = Pool(len(data_files))
+pool.map(import_csv, data_files)
