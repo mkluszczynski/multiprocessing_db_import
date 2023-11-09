f02cecca588042a1a480ddf332bf6587c99dcac7
mkluszczynski <mkluszczynski07@gmail.com>
2023-11-09 12:12:31 +0100

    Multiprocessing fix

--- __main__.py
+++ __main__.py
@@ -1,40 +1,39 @@
 from os import listdir
-from multiprocessing import Process
+from multiprocessing import Pool 
 from lib.Repository.DBRepository import DBRepository
 from utils.Config import Config
 from src.DBImporter import DBImporter
 import time
 
-dbRepo = DBRepository(
-    Config.getDbHost(),
-    Config.getDbPort(),
-    Config.getDbUser(),
-    Config.getDbPassword(),
-    Config.getDbName()
-)
 
-dbImporter = DBImporter(dbRepo)
 
 data_files = listdir("data")
 
-def importig_data(data_files):
-    for file in data_files:
-        if file == ".gitkeep": continue
-        print(file)
-        dbImporter.importDataFromCsvFile("data/" + file)
+def import_file(file):
+    if file == ".gitkeep": return
+    dbRepo = DBRepository(
+        Config.getDbHost(),
+        Config.getDbPort(),
+        Config.getDbUser(),
+        Config.getDbPassword(),
+        Config.getDbName()
+    )
+
+    dbImporter = DBImporter(dbRepo)
+    dbImporter.importDataFromCsvFile("data/" + file)
 
 
 
 if __name__ == "__main__":
     start = time.time()
-    importig_data()
+    for file in data_files:
+        import_file(file) 
     end = time.time()
-    print(f"Time taken without multiprocessing: {end-start}")
+    print(f"Time taken without multiprocessing: {end - start}")
     
-    time.sleep(1,5)
 
     start = time.time()
-    p = Process(target=importig_data, args=data_files)
-    p.start()
-    p.join()
-    print(f"Time taken using multiprocessing: {end-start}")
+    pool = Pool(len(data_files)) 
+    pool.map(import_file, data_files)
+    end = time.time()
+    print(f"Time taken using multiprocessing: {end - start}")
