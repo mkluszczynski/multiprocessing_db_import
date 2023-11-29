9485419a632eb7b682f618b14d8bb1bd63df3a37
mkluszczynski <mkluszczynski07@gmail.com>
2023-11-29 09:51:39 +0100

    Changed logic of app to import only one file

--- src/DBImporter.py
+++ src/DBImporter.py
@@ -8,37 +8,63 @@ from lib.Repository.CSVRepository import CSVRepository
 class DBImporter:
 
     __dbRepo: DBRepository
+    __table_name: str
+    __file_path: str
+    __should_log: bool
+    __csv_repo: CSVRepository
 
-    def __init__(self, db_repo: DBRepository) -> None:
+    def __init__(self, db_repo: DBRepository, table_name: str, file_path: str, shoudl_log: bool = False) -> None:
         self.__dbRepo = db_repo 
+        self.__table_name = self.__trimToTableName(table_name)
+        self.__file_path = file_path
+        self.__should_log = shoudl_log
+        csvFile = open(self.__file_path)
+        self.__csv_repo = CSVRepository(csvFile)
 
-    def importDataFromCsvFile(self, file_path: str, log: bool = False):
+    def importDataFromCsvFile(self, file_path: str):
+        
+        data = self.__csv_repo.find()
+
+        insertedRows = self.importData(data)
+        
+            
+    def createTable(self, table_name = None):
+        name = self.__table_name
+        if table_name != None: 
+            name = table_name
+
+        self.__dbRepo.createTable(name, [self.__trimColumnName(column) for column in self.__csv_repo.getColumns()])
+
+    def importData(self, data: list):
         start = time.time()
-        csvFile = open(file_path)
-        csvRepo = CSVRepository(csvFile)
-
-        tableName = self.__trimToTableName(csvFile.name)
-
-        data = csvRepo.find()
-        self.__dbRepo.createTable(tableName, [self.__trimColumnName(column) for column in csvRepo.getColumns()])
-        print(f"{Fore.CYAN}Inserting {Fore.GREEN}{file_path} {Fore.CYAN}file...")
-        insertedNo = 0
+        print(f"{Fore.CYAN}Inserting {Fore.GREEN}{self.__file_path} {Fore.CYAN}file...")
+        insertedRows = 0
         for row in data:
             rowList = self.__prepareInsertData(row)
             self.__dbRepo.add(
                     {
                         "insert": rowList,
-                        "into": tableName
+                        "into": self.__table_name 
                     }
                 )
-            insertedNo += 1
-            if log:
-                self.__logInsertMessage(str(insertedNo), file_path, tableName)
+            insertedRows += 1
+            if self.__should_log:
+                self.__logInsertMessage(str(insertedRows), self.__file_path, self.__table_name)
+                sys.stdout.write("\n")
             
-        sys.stdout.write("\n")
         end = time.time()
-        print(f"{Fore.CYAN}File {Fore.GREEN}{file_path} {Fore.MAGENTA}({str(insertedNo)} rows) {Fore.CYAN}inserted in {Fore.MAGENTA}{int(end - start)}s")
-            
+        print(f"{Fore.CYAN}File {Fore.GREEN}{self.__file_path} {Fore.MAGENTA}({str(insertedRows)} rows) {Fore.CYAN}inserted in {Fore.MAGENTA}{int(end - start)}s")
+
+        return insertedRows
+
+    def getDivData(self, sub_list_no: int):
+        data = self.__csv_repo.find()
+        sub_list_len = len(data) // sub_list_no 
+        if sub_list_len <= 0:
+            raise ValueError("Rozmiar podlisty musi być większy niż 0.")
+
+        sub_list = [data [i:i + sub_list_len] for i in range(0, len(data), sub_list_len)]
+        return sub_list
 
         # print(f"{Fore.CYAN}Inserted data from {Fore.GREEN}{file_path} {Fore.CYAN}to {Fore.GREEN}{tableName} {Fore.CYAN}table.")
     def __logInsertMessage(self, value: str, file: str, table: str):
